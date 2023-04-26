7ea5f5fd2878d8def2e2d6c6be45c3bab5383f37
Jeff Britton <jbritton@whyhotel.com>
2023-04-26 12:08:23 -0400

    printing environment variables in custom action

--- .github/actions/deploy-s3-js/index.js
+++ .github/actions/deploy-s3-js/index.js
@@ -5,6 +5,10 @@ const exec = require('@actions/exec')
 async function run() {
     core.info('Running deploy-s3-js')
 
+    // print environment variables
+    core.info(`AWS_ACCESS_KEY_ID: ${process.env.AWS_ACCESS_KEY_ID}`)
+    core.info(`AWS_SECRET_ACCESS_KEY: ${process.env.AWS_SECRET_ACCESS_KEY}`)
+
     // get input values
     const bucketName = core.getInput('bucket-name', { required: true })
     const bucketRegion = core.getInput('bucket-region', { required: true })
