541883d53fa6ae0b6f5641919d36dcd31b98930b
Jeff Britton <jbritton@whyhotel.com>
2023-04-26 11:50:13 -0400

    adding custom action inputs

--- .github/actions/deploy-s3-js/index.js
+++ .github/actions/deploy-s3-js/index.js
@@ -2,8 +2,16 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const exec = require('@actions/exec')
 
-function run() {
-    core.notice('Running deploy-s3-js')
+async function run() {
+    core.info('Running deploy-s3-js')
+
+    // get input values
+    const bucketName = core.getInput('bucket-name', { required: true })
+    const bucketRegion = core.getInput('bucket-region', { required: true })
+    const distFolder = core.getInput('dist-folder', { required: true })
+
+    // upload files
+    await exec.exec(`echo "Uploading files from ${distFolder} to S3 bucket ${bucketName} in region ${bucketRegion}"`)
 }
 
 run();
